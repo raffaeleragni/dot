e31dd102ee721cfbb95d7b40a2f0f96c0709e6cb
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-03-05 22:19:48 +0100

    fix treesitte

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -20,7 +20,7 @@ return require('packer').startup(function(use)
     end
   }
 
-  use ({'nvim-treesitter/nvim-treesitter', {run = ":TSUpdate"}})
+  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
 
   use {
     'VonHeikemen/lsp-zero.nvim',
