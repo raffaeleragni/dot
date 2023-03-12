b3609eb31768aa8f4487f501fbcc9200e70c8c9c
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-03-12 03:54:18 +0100

    shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,7 +12,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 local telescope = require('telescope.builtin')
 vim.keymap.set("n", "<leader>pf", telescope.find_files)
 
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   ensure_installed = { "c", "lua", "vim", "help", "query", "rust", "javascript", "typescript" },
   sync_install = false,
   auto_install = true,
@@ -22,15 +22,19 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
-local lsp = require('lsp-zero').preset({
+local lsp = require('lsp-zero').preset {
   name = 'minimal',
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
   suggest_lsp_servers = false,
-})
-lsp.setup_servers({'eslint', 'tsserver', 'rust_analyzer'})
-lsp.nvim_workspace()
+}
 lsp.setup()
 
+lsp.setup_servers {'eslint', 'tsserver', 'rust_analyzer'}
+lsp.nvim_workspace()
+
 require('rust-tools').setup({})
+
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader><CR>", vim.lsp.buf.code_action)
 
