1812a9ee078dc147e1c5f39ba0b106de0877398d
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-12-31 10:25:27 +0100

    move line pre setup

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -3,6 +3,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
+    use 'tpope/vim-repeat'
+    use 'tpope/vim-unimpaired'
+
     use 'ryanoasis/vim-devicons'
 
     use {
