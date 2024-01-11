b370b9f7f3074a0f6afb3cff5044b5581b049081
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-11 11:28:28 +0100

    clippy auto

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -108,6 +108,43 @@ return require('packer').startup(function(use)
     use {
         'mrcjkb/rustaceanvim',
         config = function()
+            vim.g.rustaceanvim = {
+                -- Plugin configuration
+                tools = {
+                },
+                -- LSP configuration
+                server = {
+                    on_attach = function(client, bufnr)
+                        -- you can also put keymaps in here
+                    end,
+                    settings = {
+                        -- rust-analyzer language server configuration
+                        ['rust-analyzer'] = {
+                            cargo = {
+                                --allFeatures = true,
+                                loadOutDirsFromCheck = true,
+                                runBuildScripts = true,
+                            },
+                            checkOnSave = {
+                                --allFeatures = true,
+                                command = "clippy",
+                                extraArgs = { "--no-deps" },
+                            },
+                            procMacro = {
+                                enable = true,
+                                --ignored = {
+                                --    ["async-trait"] = { "async_trait" },
+                                --    ["napi-derive"] = { "napi" },
+                                --    ["async-recursion"] = { "async_recursion" },
+                                --},
+                            },
+                        },
+                    },
+                },
+                -- DAP configuration
+                dap = {
+                },
+            }
         end
     }
 
