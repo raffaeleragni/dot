f9188f846e01f7097a7ef2a3c12b2efb0e8521d8
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-03-05 08:52:50 +0100

    always add plenary

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    use 'nvim-lua/plenary.nvim'
     use 'wbthomason/packer.nvim'
     use 'tpope/vim-repeat'
     use 'tpope/vim-unimpaired'
