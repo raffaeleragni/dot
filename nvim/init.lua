376cdce826ad4504df2dea9f2d8a676913761ec0
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-03-13 19:37:35 +0100

    revert

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,7 +92,9 @@ require("lazy").setup({
     {
         'rose-pine/neovim',
         config = function()
-            require('rose-pine').setup()
+            require('rose-pine').setup({
+                variant = "moon"
+            })
             vim.cmd("colorscheme rose-pine")
         end
     },
