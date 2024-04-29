e8172e3f0ec7db77b684d87a145cf32f87ec4dd7
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-04-30 01:39:50 +0200

    leave cursor at prev position

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,7 @@ vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
+vim.keymap.set('v', 'y', 'ygv<esc>')
 vim.keymap.set('n', 'U', ':redo<cr>')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
